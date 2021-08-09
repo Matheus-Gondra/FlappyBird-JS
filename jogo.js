5acc3dcd03c35f7668ab3d41e4e451f802acc960
Matheus-Gondra <matheusgondra951@gmail.com>
2021-08-09 18:14:51 -0300

    Criando obj do chão

--- jogo.js
+++ jogo.js
@@ -6,14 +6,46 @@ sprites.src = "./sprites.png";
 const canvas = document.querySelector("#game-canvas");
 const contexto = canvas.getContext("2d");
 
+// [Chao]
+const chao = {
+    spriteX: 0,
+    spriteY: 618,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,
+    desenha() {
+        contexto.drawImage(
+            sprites, 
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura
+        );
+    }
+}
+
+const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 24,
+    x: 18,
+    y: 58,
+    desenha() {
+        contexto.drawImage(
+            sprites, 
+            flappyBird.spriteX, flappyBird.spriteY,
+            flappyBird.largura, flappyBird.altura,
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura
+        );
+    }
+}
+
 function loop() {
-    contexto.drawImage(
-        sprites, 
-        0, 0,
-        33, 24,
-        10, 50,
-        33, 24
-    );
+
+    flappyBird.desenha();
     requestAnimationFrame(loop);
 }
 
