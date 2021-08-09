09b92f13445ab8adda5321212ee1839370b516c1
Matheus-Gondra <matheusgondra951@gmail.com>
2021-08-09 18:24:02 -0300

    Adicionando plano de fundo

--- jogo.js
+++ jogo.js
@@ -6,6 +6,37 @@ sprites.src = "./sprites.png";
 const canvas = document.querySelector("#game-canvas");
 const contexto = canvas.getContext("2d");
 
+// [Plano de Fundo]
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+    desenha() {
+        contexto.fillStyle = "#70c5ce";
+        contexto.fillRect(0, 0, canvas.width, canvas.height);
+
+        contexto.drawImage(
+            sprites, 
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+        );
+
+        contexto.drawImage(
+            sprites, 
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x + planoDeFundo.largura, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+        );
+        
+    }
+}
+
 // [Chao]
 const chao = {
     spriteX: 0,
@@ -53,8 +84,9 @@ const flappyBird = {
 
 function loop() {
 
-    flappyBird.desenha();
+    planoDeFundo.desenha();
     chao.desenha();
+    flappyBird.desenha();
     requestAnimationFrame(loop);
 }
 
